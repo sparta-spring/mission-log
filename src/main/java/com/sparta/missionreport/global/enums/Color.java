3c1c44e4c7d5d4094260bc3639cd861b5af5c0cc
jiisuniui <dudndi9752@gmail.com>
2023-12-28 12:29:20 +0900

    feat(Color): color enum 생성 (#1)

--- src/main/java/com/sparta/missionreport/global/enums/Color.java
+++ src/main/java/com/sparta/missionreport/global/enums/Color.java
@@ -1,5 +1,19 @@
 package com.sparta.missionreport.global.enums;
 
-public enum Color {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
+public enum Color {
+    NONE("none"),
+    RED("red"),
+    ORANGE("orange"),
+    YELLOW("yellow"),
+    GREEN("green"),
+    BLUE("blue"),
+    PURPLE("purple"),
+    GRAY("gray");
+
+    private final String color;
 }
