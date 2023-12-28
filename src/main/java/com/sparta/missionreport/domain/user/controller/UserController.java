c9031938e579a9c7a1df451d19f185de8de9db8d
jiisuniui <dudndi9752@gmail.com>
2023-12-28 20:18:12 +0900

    feat(User): update User Controller (#2)

--- src/main/java/com/sparta/missionreport/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/missionreport/domain/user/controller/UserController.java
@@ -3,7 +3,6 @@ package com.sparta.missionreport.domain.user.controller;
 import com.sparta.missionreport.domain.user.dto.UserDto;
 import com.sparta.missionreport.domain.user.service.UserService;
 import com.sparta.missionreport.global.common.CommonResponseDto;
-import com.sparta.missionreport.global.exception.CustomException;
 import com.sparta.missionreport.global.security.UserDetailsImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,17 +26,7 @@ public class UserController {
     public ResponseEntity<CommonResponseDto> signup(
             @Valid @RequestBody UserDto.SignupRequestDto requestDto) throws Exception {
 
-        try {
-            userService.signup(requestDto);
-        } catch (CustomException e) {
-            return ResponseEntity.status(e.getHttpStatus())
-                    .body(new CommonResponseDto(e.getHttpStatus().value(), e.getMessage(), null));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(new CommonResponseDto(HttpStatus.NOT_FOUND.value(), "예외처리되지 않은 에러입니다.",
-                            null));
-        }
+        userService.signup(requestDto);
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new CommonResponseDto(HttpStatus.CREATED.value(), "회원가입 성공", null));
@@ -47,23 +36,11 @@ public class UserController {
     public ResponseEntity<CommonResponseDto> updatePassword(
             @Valid @RequestBody UserDto.UpdatePasswordDto requestDto,
             @AuthenticationPrincipal UserDetailsImpl userDetails) throws Exception {
-        userDetails.getUsername();
-        userDetails.getUser();
-        try {
-            userService.updatePassword(requestDto, userDetails.getUser());
-        } catch (CustomException e) {
-            return ResponseEntity.status(e.getHttpStatus())
-                    .body(new CommonResponseDto(e.getHttpStatus().value(), e.getMessage(), null));
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body(new CommonResponseDto(HttpStatus.NOT_FOUND.value(), "예외처리되지 않은 에러입니다.",
-                            null));
-        }
+
+        userService.updatePassword(requestDto, userDetails.getUser());
 
         return ResponseEntity.status(HttpStatus.OK)
                 .body(new CommonResponseDto(HttpStatus.OK.value(), "비밀번호 수정 성공", null));
     }
-
 
 }
