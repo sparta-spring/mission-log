a7b75274e158a93c2f36d83f05c90516afc26c98
Kimhyeyun <cqqwer@gmail.com>
2023-12-28 17:13:36 +0900

    feat(card-worker) : 카드 작업자 여부 확인 기능 구현 (#5)

---
+++ src/main/java/com/sparta/missionreport/domain/card/service/CardWorkerService.java
@@ -0,0 +1,20 @@
+package com.sparta.missionreport.domain.card.service;
+
+import com.sparta.missionreport.domain.card.entity.Card;
+import com.sparta.missionreport.domain.card.repository.CardWorkerRepository;
+import com.sparta.missionreport.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CardWorkerService {
+
+    private final CardWorkerRepository cardWorkerRepository;
+
+    public boolean isExistedWorker(User user, Card card) {
+        return cardWorkerRepository.existsByUser_IdAndCard_Id(user.getId(), card.getId());
+    }
+}
