9bf254d8e5bcac2945f58e62ec5dabd36bfa9e2a
Kimhyeyun <cqqwer@gmail.com>
2023-12-28 13:58:10 +0900

    feat(exception) : 공통 에러 코드, 예외 핸들러 구현 (#10)

---
+++ src/main/java/com/sparta/missionreport/global/advice/GlobalExceptionHandler.java
@@ -0,0 +1,60 @@
+package com.sparta.missionreport.global.advice;
+
+import com.sparta.missionreport.global.common.CommonResponseDto;
+import com.sparta.missionreport.global.enums.GlobalExceptionCode;
+import com.sparta.missionreport.global.exception.CustomException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> runtimeExceptionHandler(RuntimeException exception) {
+        log.error("RuntimeException: ", exception);
+        return ResponseEntity.status(GlobalExceptionCode.INTERNAL_SERVER_ERROR.getHttpStatus()).body(
+                new CommonResponseDto(GlobalExceptionCode.INTERNAL_SERVER_ERROR.getHttpStatus().value(), GlobalExceptionCode.INTERNAL_SERVER_ERROR.getMessage(), null)
+        );
+    }
+
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> IllegalArgumentHandler(IllegalArgumentException exception) {
+        log.error("IllegalArgumentException: ", exception);
+        return ResponseEntity.status(GlobalExceptionCode.INVALID_VALUE.getHttpStatus()).body(
+                new CommonResponseDto(GlobalExceptionCode.INVALID_VALUE.getHttpStatus().value(), GlobalExceptionCode.INVALID_VALUE.getMessage(), null)
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> methodArgumentNotValidHandler(MethodArgumentNotValidException exception){
+        log.error("handleMethodArgumentNotValidException: ", exception);
+        BindingResult bindingResult = exception.getBindingResult();
+        Map<String, String> errors = new HashMap<>();
+        bindingResult.getAllErrors().forEach(error -> errors.put(((FieldError) error).getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.status(GlobalExceptionCode.INVALID_VALUE.getHttpStatus()).body(
+                new CommonResponseDto(GlobalExceptionCode.INVALID_VALUE.getHttpStatus().value(), GlobalExceptionCode.INVALID_VALUE.getMessage(), errors)
+        );
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<?> customExceptionHandler(CustomException exception) {
+        log.error("CustomException: ", exception);
+        return ResponseEntity.status(exception.getHttpStatus()).body(
+                new CommonResponseDto(exception.getHttpStatus().value(), exception.getMessage(), null)
+        );
+    }
+
+}
+
