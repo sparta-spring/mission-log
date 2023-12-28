aa370733558ed53ac545d6ac17aed59ab18c5ef0
jiisuniui <dudndi9752@gmail.com>
2023-12-28 20:45:36 +0900

    feat(Swagger): swagger 예제 코드 추가 (#1)

--- src/main/java/com/sparta/missionreport/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/missionreport/domain/user/controller/UserController.java
@@ -4,6 +4,8 @@ import com.sparta.missionreport.domain.user.dto.UserDto;
 import com.sparta.missionreport.domain.user.service.UserService;
 import com.sparta.missionreport.global.common.CommonResponseDto;
 import com.sparta.missionreport.global.security.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,10 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
+@Tag(name = "User Controller", description = "User Controller")
 public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "유저 회원가입 (no auth)")
     @PostMapping("/signup")
     public ResponseEntity<CommonResponseDto> signup(
             @Valid @RequestBody UserDto.SignupRequestDto requestDto) throws Exception {
@@ -32,6 +36,7 @@ public class UserController {
                 .body(new CommonResponseDto(HttpStatus.CREATED.value(), "회원가입 성공", null));
     }
 
+    @Operation(summary = "유저 비밀번호 변경")
     @PatchMapping("/password")
     public ResponseEntity<CommonResponseDto> updatePassword(
             @Valid @RequestBody UserDto.UpdatePasswordDto requestDto,
