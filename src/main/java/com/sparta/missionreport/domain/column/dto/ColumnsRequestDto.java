11143a57d638716ce388f0b7add346b384e76838
Kim-Jong-Gyu <whdrb2643@gmail.com>
2023-12-29 21:26:02 +0900

    feat(Column): Column에 Swagger 적용 (#21)

--- src/main/java/com/sparta/missionreport/domain/column/dto/ColumnsRequestDto.java
+++ src/main/java/com/sparta/missionreport/domain/column/dto/ColumnsRequestDto.java
@@ -1,27 +1,37 @@
 package com.sparta.missionreport.domain.column.dto;
 
 import com.sparta.missionreport.global.enums.Color;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 public class ColumnsRequestDto {
 
     @Getter
+    @Schema(description = "칼럼 추가 요청 dto")
     public static class AddColumnRequestDto {
+        @Schema(description = "추가할 이름", defaultValue = "test")
         private String name;
     }
 
     @Getter
+    @Schema(description = "칼럼 이름 변경 요청 dto")
     public static class UpdateColumnNameRequestDto {
+
+        @Schema(description = "변경할 이름", defaultValue = "test")
         private String name;
     }
 
     @Getter
+    @Schema(description = "칼럼 색상 변경 요청 dto")
     public static class UpdateColumnColorRequestDto {
+        @Schema(description = "변경할 색상", defaultValue = "RED")
         private Color color;
     }
 
     @Getter
+    @Schema(description = "칼럼 순서 변경 요청 dto")
     public static class UpdateColumnSequenceRequestDto {
+        @Schema(description = "변경할 순서", defaultValue = "3")
         private Long sequence;
     }
 }
