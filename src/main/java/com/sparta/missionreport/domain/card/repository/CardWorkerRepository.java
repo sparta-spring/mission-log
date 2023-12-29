7160b197ecc1f7bab190d269a3f0368223d31fea
Kimhyeyun <cqqwer@gmail.com>
2023-12-29 15:57:00 +0900

    feat(card) : queryDsl 이용해 사이 값 sequence 감소하는 코드 작성 (#5)

--- src/main/java/com/sparta/missionreport/domain/card/repository/CardWorkerRepository.java
+++ src/main/java/com/sparta/missionreport/domain/card/repository/CardWorkerRepository.java
@@ -3,7 +3,11 @@ package com.sparta.missionreport.domain.card.repository;
 import com.sparta.missionreport.domain.card.entity.CardWorker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CardWorkerRepository extends JpaRepository<CardWorker, Long> {
 
-    Boolean existsByUser_IdAndCard_Id(Long userId, Long cardId);
+    Boolean existsByUser_IdAndCard_IdAndIsDeletedIsFalse(Long userId, Long cardId);
+
+    List<CardWorker> findAllByCard_IdAndIsDeletedIsFalse(Long cardId);
 }
