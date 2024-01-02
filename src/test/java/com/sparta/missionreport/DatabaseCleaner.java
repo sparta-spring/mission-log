0e70f435e65d5574961e2ab616590465178eef0a
Kimhyeyun <cqqwer@gmail.com>
2024-01-02 17:58:04 +0900

    test(card) : 카드 서비스 생성, 수정 테스트 코드 작성 (#59)

---
+++ src/test/java/com/sparta/missionreport/DatabaseCleaner.java
@@ -0,0 +1,40 @@
+package com.sparta.missionreport;
+
+import com.google.common.base.CaseFormat;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@Profile("test")
+public class DatabaseCleaner implements InitializingBean {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private List<String> tableNames = List.of("comment", "checklists", "cards", "card_worker", "columns", "boards", "board_worker", "users");
+
+    @Override
+    public void afterPropertiesSet() {
+    }
+
+    @Transactional
+    public void execute() {
+        entityManager.flush();
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+
+        for (String tableName : tableNames) {
+            System.out.println(tableName);
+            entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
+            entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN ID RESTART WITH 1").executeUpdate();
+        }
+
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
+    }
+}
