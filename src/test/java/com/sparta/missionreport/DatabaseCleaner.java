97a436dff87a4b95fb1a5dd5a9adc185a5defa39
Kimhyeyun <cqqwer@gmail.com>
2024-01-03 11:07:38 +0900

    test(card): 작업자 초대 기능 테스트 (#59)

--- src/test/java/com/sparta/missionreport/DatabaseCleaner.java
+++ src/test/java/com/sparta/missionreport/DatabaseCleaner.java
@@ -1,7 +1,5 @@
 package com.sparta.missionreport;
 
-import com.google.common.base.CaseFormat;
-import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.InitializingBean;
@@ -10,7 +8,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 @Profile("test")
